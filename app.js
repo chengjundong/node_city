cfc5b637b8a926cf255b330bcc56709510c45758
勇敢的红胡子 <c89626@qq.com>
2016-05-14 14:12:20 +0800

    初始化项目

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/5/14.
+ */
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+   res.send('OK');
+});
+
+app.listen(3000);
